7f118cbdc0d4abe84b15ad22aaeaf40433264309
ditt-dev <206951504+ditt-dev@users.noreply.github.com>
2026-02-10 15:19:35 -0500

    Create Task UI component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import Task from "./components/ui/Task";
+// import RecipeReviewCard from "./components/ui/Task";
+
 export default function Home() {
-  return <div>Hello world.</div>;
+  return (
+    <div>
+      <Task />
+    </div>
+  );
 }
