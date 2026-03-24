ab380343de7135b5c6e37ca0d807d5910240c3bf
ditt-dev <206951504+ditt-dev@users.noreply.github.com>
2026-03-24 15:34:25 -0400

    Move drag preview logic to its own file

---
+++ app/components/ui/DragPreview.tsx
@@ -0,0 +1,59 @@
+import { createPortal } from "react-dom";
+
+import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
+import { pointerOutsideOfPreview } from "@atlaskit/pragmatic-drag-and-drop/element/pointer-outside-of-preview";
+
+import { Box } from "@mui/material";
+import type { TTask } from "./task-data";
+
+//TODO: write documentation
+
+// Render preview of dragged element.
+interface DragPreviewArgs {
+  container: HTMLElement;
+  task: TTask;
+}
+export function DragPreview({ container, task }: DragPreviewArgs) {
+  return createPortal(
+    <Box
+      sx={{
+        backgroundColor: "#ffffff",
+        color: "#000",
+        borderRadius: "0.25rem",
+        borderStyle: "solid",
+        padding: "0.5rem",
+      }}
+    >
+      {task.content}
+    </Box>,
+    container,
+  );
+}
+
+// Render the container for the drag preview.
+interface HandleDragPreviewArgs {
+  nativeSetDragImage:
+    | null
+    | ((image: HTMLElement, x: number, y: number) => void);
+
+  setState: (state: { type: "preview"; container: HTMLElement }) => void;
+}
+export function handleDragPreview({
+  nativeSetDragImage,
+  setState,
+}: HandleDragPreviewArgs) {
+  if (!nativeSetDragImage) return;
+
+  setCustomNativeDragPreview({
+    nativeSetDragImage,
+
+    // Determine the preview's offset in pixels from the mouse cursor.
+    getOffset: pointerOutsideOfPreview({
+      x: "16px",
+      y: "8px",
+    }),
+    render({ container }) {
+      setState({ type: "preview", container });
+    },
+  });
+}
--- app/components/ui/task.tsx
+++ app/components/ui/task.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-import { createPortal } from "react-dom";
 
 import {
   draggable,
   dropTargetForElements,
 } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
-import { pointerOutsideOfPreview } from "@atlaskit/pragmatic-drag-and-drop/element/pointer-outside-of-preview";
-import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
 import {
   attachClosestEdge,
   extractClosestEdge,
@@ -28,24 +25,16 @@ import DropIndicator from "@/app/components/ui/DropIndicator";
 import { getTaskData, isTaskData, type TTask } from "./task-data";
 // import { Status } from "./status";
 
-// TODO: The component passes down a hardcoded "gap" attribute. Find a way for the indicator to calculate its position without that
+// import { DragPreview, handleDragPreview } from "./DragPreview";
+
+// TODO: Separate useEffect functions for clarity
 // TODO: General code parsing and cleanup
 
 type TaskState =
-  | {
-      type: "idle";
-    }
-  | {
-      type: "preview";
-      container: HTMLElement;
-    }
-  | {
-      type: "is-dragging";
-    }
-  | {
-      type: "is-dragging-over";
-      closestEdge: Edge | null;
-    };
+  | { type: "idle" }
+  | { type: "is-dragging" }
+  | { type: "is-dragging-over"; closestEdge: Edge | null }
+  | { type: "preview"; container: HTMLElement };
 
 const idle: TaskState = { type: "idle" };
 
@@ -59,60 +48,60 @@ export function Task({ task }: { task: TTask }) {
     const element = ref.current;
 
     return combine(
+      // Function 1
       draggable({
         element,
+
         getInitialData() {
           return getTaskData(task);
         },
-        onGenerateDragPreview({ nativeSetDragImage }) {
-          setCustomNativeDragPreview({
-            nativeSetDragImage,
-            getOffset: pointerOutsideOfPreview({
-              x: "16px",
-              y: "8px",
-            }),
-            render({ container }) {
-              setState({ type: "preview", container });
-            },
-          });
-        },
+
         onDragStart() {
           setState({ type: "is-dragging" });
         },
         onDrop() {
           setState(idle);
         },
+
+        // Create a preview of the dragged element.
+        // onGenerateDragPreview({ nativeSetDragImage }) {
+        //   handleDragPreview({ nativeSetDragImage, setState });
+        // },
       }),
+
+      // Function 2
       dropTargetForElements({
         element,
+
+        // Only allow dragged element to be dropped on other task data.
         canDrop({ source }) {
-          // not allowing dropping on yourself
+          // Prevent dragged element from dropping on itself.
           if (source.element === element) {
             return false;
           }
-          // only allowing tasks to be dropped on me
           return isTaskData(source.data);
         },
+
+        // A function that returns data you want to attach to the drop target. getData() is called repeatedly while the user is dragging over the drop target in order to power addons
         getData({ input }) {
           const data = getTaskData(task);
+
           return attachClosestEdge(data, {
             element,
             input,
             allowedEdges: ["top", "bottom"],
           });
         },
+
+        // Return true if you want your drop target to hold onto selection after the user is no longer dragging over this drop target.
         getIsSticky() {
           return true;
         },
-        onDragEnter({ self }) {
-          const closestEdge = extractClosestEdge(self.data);
-          setState({ type: "is-dragging-over", closestEdge });
-        },
+
         onDrag({ self }) {
           const closestEdge = extractClosestEdge(self.data);
 
-          // Only need to update react state if nothing has changed.
-          // Prevents re-rendering.
+          // Update state only if nothing has changed to prevent re-rendering.
           setState((current) => {
             if (
               current.type === "is-dragging-over" &&
@@ -123,9 +112,16 @@ export function Task({ task }: { task: TTask }) {
             return { type: "is-dragging-over", closestEdge };
           });
         },
+
+        onDragEnter({ self }) {
+          const closestEdge = extractClosestEdge(self.data);
+          setState({ type: "is-dragging-over", closestEdge });
+        },
+
         onDragLeave() {
           setState(idle);
         },
+
         onDrop() {
           setState(idle);
         },
@@ -147,7 +143,7 @@ export function Task({ task }: { task: TTask }) {
         <Card
           onClick={handleExpandClick}
           sx={{
-            gap: "8px",
+            // gap: "8px",
             // border: "1px solid red",
             opacity: `${state.type === "is-dragging" && 0.75}`,
             // padding: "0.5rem",
@@ -196,26 +192,9 @@ export function Task({ task }: { task: TTask }) {
       </Box>
 
       {/* Display hover preview text when dragging. */}
-      {/* {state.type === "preview"
-        ? createPortal(<DragPreview task={task} />, state.container)
-        : null} */}
+      {/* {state.type === "preview" && (
+        <DragPreview task={task} container={state.container} />
+      )} */}
     </>
   );
 }
-
-// A simplified version of our task for the user to drag around
-// function DragPreview({ task }: { task: TTask }) {
-//   return (
-//     <Box
-//       sx={{
-//         backgroundColor: "#ffffff",
-//         color: "#000",
-//         borderRadius: "0.25rem",
-//         borderStyle: "solid",
-//         padding: "0.5rem",
-//       }}
-//     >
-//       {task.content}
-//     </Box>
-//   );
-// }
