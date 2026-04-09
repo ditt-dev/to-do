9ecab49f944df34603498f38d7da4f29d8424aea
ditt-dev <206951504+ditt-dev@users.noreply.github.com>
2026-04-09 19:52:44 -0400

    Implement bottom navigation bar

---
+++ app/components/ui/BottomNav.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Assignment,
+  AssignmentTurnedIn,
+  Info,
+  Settings,
+} from "@mui/icons-material";
+import {
+  Box,
+  BottomNavigation,
+  BottomNavigationAction,
+  Paper,
+} from "@mui/material";
+
+export default function BottomNav() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <Box sx={{ backgroundColor: "red" }}>
+      <Paper elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(_e, newValue) => setValue(newValue)}
+        >
+          <BottomNavigationAction
+            label="My Tasks"
+            icon={<Assignment />}
+            value={0}
+          />
+          <BottomNavigationAction
+            label="Completed"
+            icon={<AssignmentTurnedIn />}
+            value={1}
+          />
+
+          <BottomNavigationAction
+            label="Settings"
+            icon={<Settings />}
+            value={2}
+          />
+
+          <BottomNavigationAction label="About" icon={<Info />} value={3} />
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,80 @@
-import { Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
 
+import BottomNav from "@/app/components/ui/BottomNav";
 import TaskList from "@/app/components/ui/TaskList";
+import { GUTTER_WIDTH, MAX_WIDTH, MIN_HEIGHT } from "@/app/config";
+
+// BUG:
+// Internal scrolling-- need to remove overflow: hidden
 
 // TODO:
-// Add MUI paper component for box shadow
 // Check all sx attributes for unnecessary template literals
-// Export header, etc. to a layout component
 // Documentation
 
 export default function Home() {
   return (
-    <Container maxWidth="md">
-      <Stack gap={4}>
-        {/* Display header. */}
-        <Typography
-          variant="h2"
-          style={{
-            textAlign: "center",
+    <Container
+      disableGutters
+      id="app-wrapper"
+      maxWidth={MAX_WIDTH}
+      sx={{
+        display: "flex",
+        height: MIN_HEIGHT,
+        marginY: GUTTER_WIDTH,
+        position: "relative",
+      }}
+    >
+      <Paper
+        elevation={3}
+        square={false}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+
+          // Round NavBar corners.
+          overflow: "hidden",
+          width: "100%",
+        }}
+      >
+        <Box
+          id="content-wrapper"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1,
+            minHeight: 0,
+            overflow: "hidden",
+            paddingX: GUTTER_WIDTH,
           }}
         >
-          Task manager
-        </Typography>
+          {/* Display header. */}
+          <Box component="header">
+            <Typography
+              variant="h2"
+              sx={{
+                marginBottom: "2rem",
+                marginTop: "3.5rem",
+                textAlign: "center",
+              }}
+            >
+              Task manager
+            </Typography>
+          </Box>
 
-        {/* Display main. */}
-        <TaskList />
-      </Stack>
+          {/* Display task data. */}
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, minHeight: 0, overflow: "auto" }}
+          >
+            <TaskList />
+          </Box>
+        </Box>
+
+        {/* Display bottom navigation. */}
+        <Box component="nav">
+          <BottomNav />
+        </Box>
+      </Paper>
     </Container>
   );
 }
