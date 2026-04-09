f29b40d2a5592a69a716efbc06fa02a9041246d9
ditt-dev <206951504+ditt-dev@users.noreply.github.com>
2026-04-09 19:51:05 -0400

    Update global configs

--- app/config.ts
+++ app/config.ts
@@ -5,11 +5,21 @@ import { Breakpoint, createTheme } from "@mui/material";
 
 const theme = createTheme();
 
-const calcGutterWidth = (mult: number = 1) => {
+// Contains size data for media query breakpoints
+type BreakpointObject = { [key in Breakpoint]?: string };
+
+const calcGutterWidth = function (mult: number = 1): BreakpointObject {
   return { xs: theme.spacing(2 * mult), sm: theme.spacing(3 * mult) };
 };
 
-export const GUTTER_WIDTH = calcGutterWidth();
-export const GUTTER_WIDTH_2X = calcGutterWidth(2);
+const calcVertMargins = function (obj: BreakpointObject): BreakpointObject {
+  return Object.fromEntries(
+    Object.entries(obj).map(([k, v]) => [k, `calc(100dvh - 2 * ${v})`]),
+  );
+};
+
+export const GUTTER_WIDTH: BreakpointObject = calcGutterWidth();
+export const GUTTER_WIDTH_2X: BreakpointObject = calcGutterWidth(2);
 export const MAX_WIDTH: Breakpoint = "md";
+export const MIN_HEIGHT: BreakpointObject = calcVertMargins(GUTTER_WIDTH);
 export const TASK_SPACING: string = theme.spacing(1.5);
