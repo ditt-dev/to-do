a4d6a8f8a87f1dcc9cb98222d299168d4234850e
ditt-dev <206951504+ditt-dev@users.noreply.github.com>
2026-04-09 14:46:50 -0400

    Create global appWidth constant

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,9 @@
-import { createTheme } from "@mui/material";
+import { Breakpoint, createTheme } from "@mui/material";
+
+// TODO:
+// 1) Write documentation
 
 const theme = createTheme();
 
+export const appWidth: Breakpoint = "md";
 export const taskSpacing: number = parseInt(theme.spacing(1), 10);
